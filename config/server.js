6b5a8ee04700b95757da5a9df53b1e493f0784c7
misael282828 <misael282828@gmail.com>
2022-09-15 21:53:49 -0400

    Deployment 1.1

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ca0dcf33aa9ce2a378e4ad3769975689'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b2abb4b8-3d08-4257-8fef-92e278a38096'
+};
